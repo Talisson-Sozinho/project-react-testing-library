62781800a2b1e13be3820924bb95da3331b8ff86
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-06-20 11:02:20 -0300

    requisito 04 - feito os teste para o NotFound.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../pages/NotFound';
+
+const { render, screen } = require('@testing-library/react');
+
+describe('Requisito 4: Teste do NotFound.js', () => {
+  beforeEach(() => {
+    render(<NotFound />);
+  });
+
+  test('Verifica se a página contém um heading "Page requested not found"', () => {
+    const notFoundTextElement = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+
+    expect(notFoundTextElement).toBeInTheDocument();
+  });
+
+  test('Verifica se a página mostra a imagem correta', () => {
+    const imageUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imgElement = screen.getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+
+    expect(imgElement).toHaveAttribute('src', imageUrl);
+  });
+});
